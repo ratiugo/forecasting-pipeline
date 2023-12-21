68c9ae639876ae65b4d8b34056c767ee90a91587
Colton Williams <coltonwilliams@Coltons-MacBook-Pro-2.local>
2023-12-20 16:39:53 -0800

    :construction: WIP

--- forecast/pre_process.py
+++ forecast/pre_process.py
@@ -3,15 +3,96 @@ Pipeline to process the input dataset and configuration files, preparing for the
 pipeline.
 """
 from forecast.utils import S3Utility
+from os import environ
+from typing import Union
+import pandas as pd
 
-class PreProcess:
-	"""
+BUCKET = environ.get("FORECAST_S3_BUCKET")
 
+class PreProcess(S3Utility):
 	"""
+	Process the CSV dataset into the shape required by the forecasting lambda
+	"""
+	def __init__(self, **kwargs):
+		self.config = self.get_s3_object(bucket=bucket, key=kwargs.get("config_file"))
+		self.dataset = None
+
+
+	def run(self) -> PreProcess:
+		"""
+		Run the pipeline
+		"""
+		(
+			self.load_dataset()
+				.create_model_dataset()
+		)
+
+		return self
+
+	def load_dataset(self) -> PreProcess:
+		"""
+		Read the S3 location of the CSV dataset and load it as a pandas dataframe
+		"""
+		self.dataset = {
+			"meta_data": self.config["Dataset"]["meta_data"]
+			"data": pd.read_csv(self.config["Dataset"]["file_name"])
+		} 
+
+		return self
+
+	def create_model_dataset(self) -> PreProcess:
+		"""
+		Create individual model datasets
+		"""
+		model_dataset = {"config": self.config["Config"]}
+		model_dataset.update(self.dataset)
+
+		if "group_models_by" in model_dataset["meta_data"].keys():
+			model_dataset["model_group"] = {}
+			model_dataset = [self.split_model_dataset(model_dataset)]
+
+	@staticmethod
+	def slice_dataset(model: dict, groups: np.ndarray, column: str) -> dict:
+		"""
+		Split a dataset into individual dataframes 
+		"""
+		data = model["data"]
+		return {group: data.loc[data[column] == group, data.columns != column] for group in groups}
+
+	def split_model_dataset(self, model: dict) -> Union[list, dict]:
+		"""
+
+		"""
+		columns = model["meta_data"]["group_models_by"].copy()
+		if columns:
+			group_by_col = columns.pop()
+			groups = model["data"][group_by_col].unique()
+
+			grouped_data = self.slice_dataset(model, groups, group_by_col)
+
+			models = []
+			for group in groups:
+				model_group = model["model_group"].copy()
+				model_group[group_by_col] = group
+				model_tmp = {
+					"config": model["config"],
+					"model_group": model_group,
+					"meta_data": model["meta_data"],
+					"data": grouped_data[group]
+				}
+				models.append(model_tmp)
+
+			return_model = [self.split_model_dataset(mod) for mod in models]
+		else:
+			return model = model
+
+		return return_model
+
+
 
 def pre_process_handler(event: Any, context: Any) -> str:
 	"""
 	Lambda handler to run the preprocess pipeline
 	"""
-	
+	return PreProcess(config_file=event.get("config_file")).run()
 
--- tools/run_forecasting_tool.py
+++ tools/run_forecasting_tool.py
@@ -17,9 +17,9 @@ class RunForecastStepFunction:
 	Run the forecast step function after staging the data and configuration files to S3.
 	"""
 	def __init__(self, **kwargs):
-		if not self.config_file_name.endswith(".json"):
+		if not kwargs.get("config_file_name").endswith(".json"):
 			raise ValueError("Invalid config file format. Must be a JSON file.")
-		if not self.data_file_name.endswith(".csv"):
+		if not kwargs.get("data_file_name").endswith(".csv"):
 			raise ValueError("Invalid data file format. Must be a CSV file.")
 
 		self.config_file_name = kwargs.get("config_file_name")
@@ -35,7 +35,9 @@ class RunForecastStepFunction:
 		"""
 		(
 			self.generate_process_id()
+				.update_forecast_configuration_with_dataset_location()
 				.stage_input_files()
+				.run_step_function()
 		)
 		return self
 
@@ -46,8 +48,15 @@ class RunForecastStepFunction:
 		timestamp = datetime.utcnow().strftime("%Y%m%d")
 		unique_id = uuid.uuid1()
 		self.process_id = f"{timestamp}-{unique_id}"
+		self.config["process_id"] = self.process_id
 
 		return self
+
+	def update_forecast_configuration_with_dataset_location(self) -> RunForecastStepFunction:
+		"""
+		Add the S3 file path of the dataset to the configuration file
+		"""
+		self.config["Dataset"]["file_name"] = f"{self.process_id}/input/{self.data_file_name}"
 
 	@staticmethod
 	def stage_data_to_s3(body: Any, key: str) -> None:
@@ -66,13 +75,11 @@ class RunForecastStepFunction:
 		self.data.to_csv(csv_buffer, index=False)
 		config = bytes(json.dumps(self.config, indent=4).encode("UTF-8"))
 
-		RunForecastStepFunction.stage_data_to_s3(
-			body = csv_buffer.getvalue(),
-			key = f"s3://{bucket}/{self.process_id}/input/{self.data_file_name}"
+		self.stage_data_to_s3(body=csv_buffer.getvalue(),
+							  key=f"{self.process_id}/input/{self.data_file_name}"
 		)
-		RunForecastStepFunction.stage_data_to_s3(
-			body = config,
-			key = f"s3://{bucket}/{self.process_id}/input/{self.config_file_name}"
+		self.stage_data_to_s3(body=config,
+							  key=f"{self.process_id}/input/{self.config_file_name}"
 		)
 
 		return self
@@ -82,10 +89,24 @@ class RunForecastStepFunction:
 		Run the forecasting step function, and return the status
 		"""
 		state_machine_arn = environ.get("STATE_MACHINE_ARN")
-		
+		session = boto3.Session(profile_name=os.environ.get("AWS_PROFILE", "default"))
+		client = session.client("stepfunctions")
 
+		input_json = f"{{'config_file': '{f"{self.process_id}/input/{self.config_file_name}"}'}}"
 
+		response = client.start_execution(
+			stateMachineArn=state_machine_arn, input=input+json
+		)
+        execution_arn = response.get("executionArn")
+        wait_time = 10
+        status = "RUNNING"
 
+        while status == "RUNNING":
+        	print("Running...")
+            time.sleep(wait_time)
+            status = client.describe_execution(executionArn=execution_arn)["status"]
+
+        print(f"Process {self.process_id} finished with status: {status}.")
 
 
 
